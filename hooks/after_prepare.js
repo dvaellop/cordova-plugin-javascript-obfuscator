3d55e960f62758e38c5c238045c9de96129b034c
dvaellop <63355947+dvaellop@users.noreply.github.com>
2021-03-18 19:09:25 +0100

    Update after_prepare.js

--- hooks/after_prepare.js
+++ hooks/after_prepare.js
@@ -13,11 +13,7 @@ module.exports = function(context) {
 
     var targetFiles = loadObfuscatedFileTargets();
 
-    context.opts.platforms.filter(function(platform) {
-        var pluginInfo = context.opts.plugin.pluginInfo;
-        return pluginInfo.getPlatformsArray().indexOf(platform) > -1;
-        
-    }).forEach(function(platform) {
+    context.opts.platforms.forEach(function(platform) {
         var platformPath = path.join(projectRoot, 'platforms', platform);
         var platformApi = platforms.getPlatformApi(platform, platformPath);
         var platformInfo = platformApi.getPlatformInfo();
